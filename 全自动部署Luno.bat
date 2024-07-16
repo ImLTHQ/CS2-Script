fd1329fb699a3090523da9e08040bf37d3f23eaa
ImXiaoYe <139523852+ImLTHQ@users.noreply.github.com>
2024-07-17 03:51:50 +0800

    Add files via upload

--- 全自动部署Luno.bat
+++ 全自动部署Luno.bat
@@ -1,14 +1,34 @@
-:: 设置
 @echo off
-title luno懒人脚本(自动获取最新luno，作者自调参数并启动luno)
+title 全自动部署Luno
+:: 变量
 set lunoURL=https://lunocs2.ru/request?downloadLoader
 set configURL=https://github.com/ImLTHQ/CS2-Script/releases/download/main/luno-main.cfg
+set cfgURL=https://lunocs2.ru/request?downloadConfigs
 set folder=C:\Luno
-echo 文件释放目录"%folder%"
-echo.
 
-:: 检查
+::  检查CS
+:checkCS
+tasklist|find /i "cs2.exe"
+
+if %errorlevel%==0 (
+    cls
+    GOTO :checkFolder
+) else (
+    cls
+    GOTO :startCS
+)
+
+:: 启动CS
+:startCS
+echo 启动CS
+echo.
+powershell start "steam://rungameid/730"
+GOTO :checkFolder
+
+:: 检查目录
 :checkFolder
+echo 文件释放目录"%folder%"
+
 if exist "%folder%" (
 GOTO :download
 ) else (
@@ -20,26 +40,40 @@ GOTO :download
 
 :: 下载
 :download
+echo.
 echo 下载Luno
-powershell wget -o %folder%\LunoLoader.exe %lunoURL% && echo 成功 || GOTO :fail
+powershell wget -o %folder%\LunoLoader.exe %lunoURL% && echo - 成功 || GOTO :fail
+
 echo.
 echo 下载参数
-powershell wget -o %folder%\luno-main.cfg %configURL% && echo 成功 || echo 失败，仍旧执行下一步
+powershell wget -o %folder%\luno-main.cfg %configURL% && echo - 成功 || echo - 失败，仍旧执行下一步
+
+echo.
+echo 从官网下载参数
+powershell wget -o %folder%\configs.zip %cfgURL% && echo - 成功 || echo - 失败，仍旧执行下一步
+echo - 解压缩参数
+tar -xvf %folder%\configs.zip -C %folder%
+echo 删除无用文件
+del /Q %folder%\configs.zip
+del /Q %folder%\"ОБЯЗАТЕЛЬНО К ПРОЧТЕНИЮ!!!.txt"
 
 :: 启动
 echo.
 echo 准备启动luno，蓝色按钮是注入（注意：把默认启用按钮关了！开启时注入会安装第三方浏览器）
 echo GUI快捷键是ins,齿轮图标是设置，点进去选择luno-main，然后点load加载参数
+echo.
 %folder%\LunoLoader.exe && GOTO :end
 
 :: Luno下载失败
 :fail
+
 echo.
-echo LunoLoader下载失败，请检查网络重试，必要时可以使用网络代理
+echo - LunoLoader下载失败，请检查网络重试，必要时可以使用网络代理
 pause
 exit
 
 :: 结束
 :end
+
 pause
 exit
--- 全自动部署Valthrun.bat
+++ 全自动部署Valthrun.bat
@@ -1,6 +1,7 @@
 :: 设置
 @echo off
 title 全自动部署Valthrun
+:: 变量
 set controllerURL=https://github.com/Valthrun/Valthrun/releases/download/v0.4.4/controller_fb6b96a.exe
 set radarURL=https://github.com/Valthrun/Valthrun/releases/download/v0.4.4/radar_client_fb6b96a.exe
 set kdmapperURL=https://github.com/valthrunner/Valthrun/releases/download/4/kdmapper.exe
@@ -8,39 +9,62 @@ set driverURL=https://github.com/Valthrun/Valthrun/releases/download/v0.4.4/valt
 set folder=D:\CS2-Valthrun
 set githubURL=https://github.com/ImLTHQ/CS2-Script
 
-echo 文件释放目录“%folder%”
-echo · Valthrun Wiki “https://wiki.valth.run/zh-cn/”
-echo · 为了让 Valthrun 正常工作，必须禁用一些Windows 安全功能。请参考“https://wiki.valth.run/zh-cn/troubleshooting/kernel/windows_security_features”
+::  检查CS
+:checkCS
+tasklist|find /i "cs2.exe"
+
+if %errorlevel%==0 (
+    cls
+    GOTO :checkFolder
+) else (
+    cls
+    GOTO :startCS
+)
+
+:: 启动CS
+:startCS
+echo 启动CS
 echo.
+powershell start "steam://rungameid/730"
+GOTO :checkFolder
 
 :: 检查
+:checkFolder
+echo 文件释放目录"%folder%"
+echo · Valthrun Wiki https://wiki.valth.run/zh-cn/
+echo · 为了让 Valthrun 正常工作，必须禁用一些Windows 安全功能。请参考 https://wiki.valth.run/zh-cn/troubleshooting/kernel/windows_security_features
+
 if exist "%folder%" (
 GOTO :downloadController
 ) else (
+    echo.
     echo 未找到%folder%,正在创建
     md %folder% && echo 创建成功
-    echo.
     GOTO :downloadController
 )
 
 :: 下载
 :downloadController
 if exist "%folder%\controller.exe" GOTO :downloadRadar
+echo.
 echo 下载controller %controllerURL%
 powershell wget -o %folder%\controller.exe %controllerURL% && echo 成功 || echo 失败 && GOTO :end
 
 :downloadRadar
 if exist "%folder%\radar.exe" GOTO :downloadKdmapper
+echo.
 echo 下载雷达 %radarURL%
 powershell wget -o %folder%\radar.exe %radarURL% && echo 成功 || echo 失败 && GOTO :end
 
 :downloadKdmapper
 if exist "%folder%\kdmapper.exe" GOTO :downloadDriver
+echo.
 echo 下载kdmapper %kdmapperURL%
 powershell wget -o %folder%\kdmapper.exe %kdmapperURL% && echo 成功 || echo 失败 && GOTO :end
 
 :downloadDriver
 if exist "%folder%\driver.sys" GOTO :loadDriver
+echo.
 echo 下载驱动 %driverURL%
 powershell wget -o %folder%\driver.sys %driverURL% && echo 成功 || echo 失败 && GOTO :end
 
@@ -55,12 +79,13 @@ GOTO :errorKdmapper
 :errorKdmapper
 echo.
 echo 失败，请以管理员身份重新运行脚本，并截图寻求他人帮助
+echo 出现“系统无法执行指定的程序”可能是Windows安全中心报毒隔离
 echo 请观察终端，找到 [+] DriverEntry returned "内容"
-echo 内容为0xCF000004 是Valthrun 驱动程序已加载
-echo 内容为0xCF000003 是Valthrun 驱动程序初始化失败。详情请查看 DebugView。
-echo 内容为0xCF000002 是设置 Valthrun 驱动程序的函数调用失败。详情请查看 DebugView。
-echo 内容为0xCF000001 是Valthrun 日志系统无法初始化。这种情况很罕见
-echo 内容为0xc0000603 很可能是由于使用 intel 驱动程序作为易受攻击的驱动程序 （kdmapper issue #65）。请尝试在“Windows安全中心>设备安全性>内核隔离详细信息”中禁用“易受攻击的驱动程序阻止列表”
+echo - 内容为0xCF000004 是Valthrun 驱动程序已加载
+echo - 内容为0xCF000003 是Valthrun 驱动程序初始化失败。
+echo - 内容为0xCF000002 是设置 Valthrun 驱动程序的函数调用失败。
+echo - 内容为0xCF000001 是Valthrun 日志系统无法初始化。这种情况很罕见
+echo - 内容为0xc0000603 请尝试在“Windows安全中心>设备安全性>内核隔离详细信息”中禁用“易受攻击的驱动程序阻止列表”
 GOTO :end
 
 :: 选择
@@ -83,18 +108,18 @@ if "%INPUT%" == "1" (
 ) else if "%INPUT%" == "3" (
     GOTO :help
 ) else if "%INPUT%" == "4" (
-    exit
+    GOTO :end
 ) else (
-    exit
+    GOTO :end
 )
 
 :: 帮助
 :help
 echo.
-echo · 没有pause键请参考“https://wiki.valth.run/zh-cn/troubleshooting/overlay/pause_key”
+echo · 没有pause键请参考 https://wiki.valth.run/zh-cn/troubleshooting/overlay/pause_key
 echo · AMD用户在运行Controller后游戏黑屏请使用专业版显卡驱动或将显卡驱动降级至23.7.1
-echo · 终端显示“Failed to load vulkan-1.dll (os error 14001)” 请参考“https://wiki.valth.run/zh-cn/troubleshooting/overlay/amd_opengl” 方案2，3，5
-echo · 更多加载驱动方法请参考“https://wiki.valth.run/zh-cn/getting-started/driver/”
+echo · 终端显示“Failed to load vulkan-1.dll (os error 14001)” 请参考 https://wiki.valth.run/zh-cn/troubleshooting/overlay/amd_opengl 方案2，3，5
+echo · 更多加载驱动方法请参考 https://wiki.valth.run/zh-cn/getting-started/driver/
 GOTO :main
 
 :: 启动
@@ -104,14 +129,14 @@ echo 启动controller
 echo · GUI快捷键是pause。
 echo · Valthrun不支持CS2全屏模式
 %folder%\controller.exe
-echo 非正常退出,代码%ERRORLEVEL%。可能是没有启动CS2就启动controller导致的
+echo 非正常退出,代码%ERRORLEVEL%。
 GOTO :main
 
 :loadRadar
 echo.
 echo 启动雷达
 %folder%\radar.exe
-echo 非正常退出,代码%ERRORLEVEL%。可能是没有启动CS2就启动雷达导致的
+echo 非正常退出,代码%ERRORLEVEL%。
 GOTO :main
 
 :: 结束
